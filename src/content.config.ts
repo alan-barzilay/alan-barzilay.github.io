d76bcc56f71e7d1194ac84e398ccdc96e04edbbf
Alan Barzilay <alan.barzilay@gmail.com>
2025-12-10 12:39:02 -0300

    posts: fix date parsing

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,7 +13,10 @@ const blog = defineCollection({
     img_src: image().optional(),
     alt_text: z.string().optional(),
     tags: z.array(z.string()),
-    date: z.date(),
+    date: z.string().transform((str) => {// i hate this, fucking stupid mmddyyyy default
+      const [day, month, year] = str.split(/[\/-]/).map(Number); // Split by slash or dash
+      return new Date(year, month - 1, day); //cursed JavaScript's 0-indexed months. // WHAT THE ACTUAL FUCK
+    }),
     minutes: z.string().optional(),
     paragrafo: z.string(),
     draft: z.boolean().optional().default(false),
